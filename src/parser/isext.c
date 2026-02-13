34cad53b9c86aee89b7311774714c36901a40c52
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2026-02-13 14:15:45 -0300

    refact: rename to isext

--- src/parser/isext.c
+++ src/parser/isext.c
@@ -1,25 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   is_valid_ext.c                                     :+:      :+:    :+:   */
+/*   isext.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
+/*   By: mhidani <mhidani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/11 20:16:36 by mhidani           #+#    #+#             */
-/*   Updated: 2026/02/11 20:20:53 by mhidani          ###   ########.fr       */
+/*   Updated: 2026/02/13 14:15:16 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-t_bool	is_valid_ext(char *file, char *ext)
+t_bool	isext(char *file, char *ext)
 {
-	size_t	ext_size;
+	size_t	extension_size;
 	size_t	to_target;
 
 	if (!file || !ext)
 		return (FALSE);
-	ext_size = ft_strlen(ext);
-	to_target = ft_strlen(file) - ext_size;
-	return (ft_strncmp(file + to_target, ext, ext_size) == 0);
+	extension_size = ft_strlen(ext);
+	to_target = ft_strlen(file) - extension_size;
+	return (ft_strncmp(file + to_target, ext, extension_size) == 0);
 }
