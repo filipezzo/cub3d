377b20d18cdce423a0660c9ebc2ac51c86a9a9ed
filipezzo <fastnexi@gmail.com>
2026-02-18 16:05:00 -0300

    fix: engine norminette

--- src/engine/engine_shutdown.c
+++ src/engine/engine_shutdown.c
@@ -1,21 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   engine_shutdown.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fsousa <fsousa@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/18 16:00:32 by fsousa            #+#    #+#             */
+/*   Updated: 2026/02/18 16:00:33 by fsousa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub3d.h"
 
-void engine_shutdown(t_engine *e)
+void	engine_shutdown(t_engine *e)
 {
-    if (e->frame.img)
-    {
-        mlx_destroy_image(e->mlx, e->frame.img);
-        e->frame.img = NULL;
-    }
-    if (e->win)
-    {
-        mlx_destroy_window(e->mlx, e->win);
-        e->win = NULL;
-    }
-    if (e->mlx)
-    {
-        mlx_destroy_display(e->mlx);
-        free(e->mlx);
-        e->mlx = NULL;
-    }
+	if (e->frame.img)
+	{
+		mlx_destroy_image(e->mlx, e->frame.img);
+		e->frame.img = NULL;
+	}
+	if (e->win)
+	{
+		mlx_destroy_window(e->mlx, e->win);
+		e->win = NULL;
+	}
+	if (e->mlx)
+	{
+		mlx_destroy_display(e->mlx);
+		free(e->mlx);
+		e->mlx = NULL;
+	}
 }
--- src/engine/engine_tick.c
+++ src/engine/engine_tick.c
@@ -6,16 +6,17 @@
 /*   By: fsousa <fsousa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/13 12:34:25 by fsousa            #+#    #+#             */
-/*   Updated: 2026/02/13 12:34:25 by fsousa           ###   ########.fr       */
+/*   Updated: 2026/02/18 16:04:32 by fsousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int engine_tick(void *param)
+int	engine_tick(void *param)
 {
-	t_game *game = (t_game *)param;
+	t_game	*game;
 
+	game = (t_game *)param;
 	if (!game)
 		return (0);
 	if (!game->eng.running)
@@ -23,10 +24,9 @@ int engine_tick(void *param)
 		engine_shutdown(&game->eng);
 		exit(0);
 	}
-
 	player_update(game);
 	render_minimap(game);
-
-	mlx_put_image_to_window(game->eng.mlx, game->eng.win, game->eng.frame.img, 0, 0);
+	mlx_put_image_to_window(game->eng.mlx, game->eng.win, game->eng.frame.img,
+		0, 0);
 	return (0);
 }
