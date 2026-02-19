a209c2f25692141d80223a7ce27677f9f552e6e6
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2026-02-19 15:56:57 -0300

    refact: return file descritor

--- src/parser/validate_fpath.c
+++ src/parser/validate_fpath.c
@@ -6,26 +6,25 @@
 /*   By: mhidani <mhidani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/13 15:31:07 by mhidani           #+#    #+#             */
-/*   Updated: 2026/02/13 15:36:40 by mhidani          ###   ########.fr       */
+/*   Updated: 2026/02/19 09:45:17 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-t_bool	validate_fpath(char *fpath, char *ext, int *fd)
+int	validate_fpath(char *fpath, char *ext)
 {
+	int	fd;
+
 	if (!fpath || !ext)
 		return (FALSE);
 	if (!isext(fpath, ext))
 	{
 		perr("The file extension provided is invalid");
-		return (FALSE);
+		return (-1);
 	}
-	*fd = open(fpath, O_RDONLY);
+	fd = open(fpath, O_RDONLY);
 	if (fd == -1)
-	{
 		perr("Could not find the map file");
-		return (FALSE);
-	}
-	return (TRUE);
+	return (fd);
 }
