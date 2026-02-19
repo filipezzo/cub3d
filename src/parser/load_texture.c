b8c85be1e420af5940a1681ad10f2d84ef402813
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2026-02-19 15:56:13 -0300

    refact: check fd and close

--- src/parser/load_texture.c
+++ src/parser/load_texture.c
@@ -6,7 +6,7 @@
 /*   By: mhidani <mhidani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/13 15:45:02 by mhidani           #+#    #+#             */
-/*   Updated: 2026/02/13 15:58:07 by mhidani          ###   ########.fr       */
+/*   Updated: 2026/02/19 10:39:13 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,14 @@ t_bool	load_texture(t_engine *engine, t_data *tex, char *path)
 
 	if (!tex || !path)
 		return (FALSE);
-	fd = 0;
 	path = ft_strtrim(path, " \t\n");
-	if (!validate_fpath(path, ".xpm", &fd))
-	{
-		safe_close_fd(fd);
+	fd = validate_fpath(path, ".xpm");
+	if (fd == -1)
 		exit(EXIT_FAILURE);
-	}
 	safe_close_fd(fd);
 	tex->img = mlx_xpm_file_to_image(engine->mlx, path, &tex->w, &tex->h);
 	if (!tex->img)
-	{
-		perr("Failed to load texture");
-		return (FALSE);
-	}
+		return (perr_failed("Failed to MLX load texture"));
 	tex->addr = mlx_get_data_addr(
 		tex->img,
 		&tex->bits_per_pixel,
