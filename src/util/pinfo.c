5a2f8729e6173749860a11d7c1b4079c4236414d
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2026-02-20 01:55:58 -0300

    feat: print info

--- src/util/pinfo.c
+++ src/util/pinfo.c
@@ -6,7 +6,7 @@
 /*   By: mhidani <mhidani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/19 19:51:42 by mhidani           #+#    #+#             */
-/*   Updated: 2026/02/19 20:07:10 by mhidani          ###   ########.fr       */
+/*   Updated: 2026/02/20 01:48:08 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@ void	pinfo(const char *msg)
 	if (!msg || !*msg)
 		return ;
 	ft_putstr_fd("[info] ", STDOUT_FILENO);
-	ft_putstr_fd(msg, STDOUT_FILENO);
+	ft_putstr_fd((char *)msg, STDOUT_FILENO);
 	ft_putstr_fd("\n", STDOUT_FILENO);
 }
